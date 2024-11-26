21604ff50f25d3f938dc753ea89472db3a792eaa
Nurian Coelho <nuriancoelho@hotmail.com>
2024-11-26 11:13:57 -0300

    refactor(global): query provider

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClientProvider } from '@tanstack/react-query';
 
+import { queryClient } from '~/configs';
 import Routes from '~/routes';
 import { light } from '~/styles/theme';
 
@@ -18,16 +20,18 @@ if (__DEV__ && process.env.JEST_WORKER_ID == null) {
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <ThemeProvider theme={light}>
-        <StatusBar
-          backgroundColor="transparent"
-          translucent
-          barStyle="light-content"
-        />
-        <SafeAreaProvider>
-          <Routes />
-        </SafeAreaProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={light}>
+          <StatusBar
+            backgroundColor="transparent"
+            translucent
+            barStyle="light-content"
+          />
+          <SafeAreaProvider>
+            <Routes />
+          </SafeAreaProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </NavigationContainer>
   );
 }
